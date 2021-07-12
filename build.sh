31766c03b76dd916856caca8935d67aed9cbc1a2
Nizar Rian <garudaindonesia710@gmail.com>
2021-07-12 15:22:53 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,15 +7,15 @@ source ~/.bashrc && source ~/.profile
 export LC_ALL=C && export USE_CCACHE=1
 ccache -M 100G
 export ARCH=arm64
-export KBUILD_BUILD_HOST=neolit
-export KBUILD_BUILD_USER="sarthakroy2002"
+export KBUILD_BUILD_HOST=ubuntu
+export KBUILD_BUILD_USER="rian1337"
 git clone --depth=1 https://github.com/sarthakroy2002/clang-r353983c1 clang
 git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
 git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
 
 [ -d "out" ] && rm -rf out || mkdir -p out
 
-make O=out ARCH=arm64 rmx2020_defconfig
+make O=out ARCH=arm64 rmx2020nh_defconfig
 
 PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}" \
 make -j$(nproc --all) O=out \
